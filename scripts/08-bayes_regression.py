a02f38dfac2774e4ff1146f38a7b8c3f7a4326b5
ahnsun98 <61009770+ahnsun98@users.noreply.github.com>
2021-04-13 21:07:24 +0900

    Update 08-bayes_regression.py

--- scripts/08-bayes_regression.py
+++ scripts/08-bayes_regression.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-#
-# ベイズ推定による回帰分析
-#
-# 2015/06/23 ver1.0
-#
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,11 +7,11 @@ from pandas import Series, DataFrame
 
 from numpy.random import normal, multivariate_normal
 
-beta = 1.0/(0.3)**2     # 真の分布の分散
-alpha = 1.0/100**2      # 事前分布の分散
-order = 9               # 多項式の字数
+beta = 1.0/(0.3)**2     # 실제 분포의 분산
+alpha = 1.0/100**2      # 사전 분포의 분산
+order = 9               # 다항식 차수
 
-# データセット {x_n,y_n} (n=1...N) を用意
+# data set {x_n,y_n} (n=1...N) 준비
 def create_dataset(num):
     dataset = DataFrame(columns=['x','y'])
     for i in range(num):
@@ -26,7 +21,7 @@ def create_dataset(num):
                                  ignore_index=True)
     return dataset
 
-# 事後分布に基づく推定曲線、および、事後分布の平均と分散を計算
+# 사후 분포에 기초한 추정곡선 및 사후 분포의 평균과 분산을 계산
 def resolve(dataset, m):
     t = dataset.y
     phis = DataFrame()
@@ -42,9 +37,9 @@ def resolve(dataset, m):
         else:
             phiphi += np.dot(phi,phi.T)
     s_inv = alpha * DataFrame(np.identity(m+1)) + beta * phiphi
-    s = np.linalg.inv(s_inv) # 事後分布の共分散行列
+    s = np.linalg.inv(s_inv) # 사후 분포의 공분산 행렬
 
-    # 平均 m(x)
+    # 평균 m(x)
     def mean_fun(x0):
         phi_x0 = DataFrame([x0 ** i for i in range(0,m+1)])
         for index, line in phis.iterrows():
@@ -54,7 +49,7 @@ def resolve(dataset, m):
                 tmp += t[index] * line
         return (beta * np.dot(np.dot(phi_x0.T, s), DataFrame(tmp))).flatten()
 
-    # 標準偏差 s(x)
+    # 표준 편차 s(x)
     def deviation_fun(x0):
         phi_x0 = DataFrame([x0 ** i for i in range(0,m+1)])
         deviation = np.sqrt(1.0/beta + np.dot(np.dot(phi_x0.T, s), phi_x0))
@@ -65,17 +60,17 @@ def resolve(dataset, m):
             tmp = t[index] * line
         else:
             tmp += t[index] * line
-    mean = beta * np.dot(s, DataFrame(tmp)).flatten() # 事後分布の平均
+    mean = beta * np.dot(s, DataFrame(tmp)).flatten() # 사후 분포 평균
 
     return mean_fun, deviation_fun, mean, s
 
-# Main
+
 if __name__ == '__main__':
     df_ws = DataFrame()
 
     fig1 = plt.figure()
     fig2 = plt.figure()
-    for c, num in enumerate([4,5,10,100]): # トレーニングセットのデータ数
+    for c, num in enumerate([4,5,10,100]): # training set data 수 (N)
         train_set = create_dataset(num)
         mean_fun, deviation_fun, mean, sigma = resolve(train_set, order)
         ws_samples = DataFrame(multivariate_normal(mean,sigma,4))
@@ -90,17 +85,17 @@ if __name__ == '__main__':
         subplot2.set_ylim(-2,2)
         subplot2.set_title("N=%d" % num)
 
-        # トレーニングセットを表示
+        # training set 표시
         subplot1.scatter(train_set.x, train_set.y, marker='o', color='blue')
         subplot2.scatter(train_set.x, train_set.y, marker='o', color='blue')
 
         linex = np.arange(0,1.01,0.01)
 
-        # 真の曲線を表示
+        # 실제 곡선 표시
         liney = np.sin(2*np.pi*linex)
         subplot1.plot(linex, liney, color='green',linestyle=':')
 
-        # 平均と標準偏差の曲線を表示
+        # 평균과 표준편차의 곡선을 표시
         m = np.array(mean_fun(linex))
         d = np.array(deviation_fun(linex))
         subplot1.plot(linex, m, color='red', label='mean')
@@ -108,7 +103,7 @@ if __name__ == '__main__':
         subplot1.plot(linex, m-d, color='black', linestyle='--')
         subplot1.plot(linex, m+d, color='black', linestyle='--')
 
-        # 多項式のサンプルを表示
+        # 다항식 샘플 표시
         m = np.array(mean_fun(linex))
         d = np.array(deviation_fun(linex))
         liney = m
@@ -127,4 +122,3 @@ if __name__ == '__main__':
 
     fig1.show()
     fig2.show()
-
